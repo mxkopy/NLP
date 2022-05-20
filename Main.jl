8ec3ae6c4111951b9864d5837f1dd88879267b9e
mxkopy <mxkopy@bigcompute.lan>
2022-05-20 00:41:27 -1000

    separated training/init commands for audio & video autoencoders

--- Main.jl
+++ Main.jl
@@ -8,13 +8,17 @@ function arguments()
 
     @add_arg_table s begin 
 
-        "--init"
-            help   = "reinitializes & saves autoencoders"
+        "--init-audio"
             action = :store_true
 
-        "--train-autoencoders"
-            help   = "trains autoencoders on the data/audio & data/video directories"
+	"--init-video"
+	    action = :store_true
+
+        "--train-audio"
             action = :store_true
+	
+	"--train-video"
+	    action = :store_true
 
     end
 
@@ -24,24 +28,27 @@ end
 
 args = arguments()
 
-if args["init"]
+if args["init-audio"]
 
     init_model( create_audio_autoencoder(), "data/models/audio.bson" )
-    println("audio model initialized")
-
-    init_model( create_video_autoencoder(), "data/models/video.bson" )
-    println("video model initialized")
-
 
 end
 
-if args["train-autoencoders"]
+if args["init-video"]
+
+    init_model( create_video_autoencoder(), "data/models/video.bson" )
+
+end
+
+if args["train-audio"]
 
     train_audio()
-    println("audio model training")
+
+end
+
+
+if args["train-video"]
 
     train_video()
-    println("video model training")
-
 
 end
