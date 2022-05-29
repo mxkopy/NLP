99d572f70e9463065ee6cd16cd21936fb579b7f7
mxkopy <mxkopylov@gmail.com>
2022-05-29 04:23:42 -0700

    fixed model saving as gpu, maybe

--- Training.jl
+++ Training.jl
@@ -78,19 +78,19 @@ function train_autoencoder( model_dir, data_dir, data_itr, reshaper )
 
     model, parameters, opt = load_model( model_dir )
 
-    model                  = model .|> gpu
-
     num_files = length(readdir(data_dir))
 
     for (i, dir) in enumerate(readdir(data_dir, join=true))
+
+        model = model .|> gpu
 
         println("$i of $num_files")
 
         train_over_data( model, parameters, opt, data_itr(dir), reshaper )
 
-        model = model .|> cpu
+        cpu_model = model .|> cpu
 
-        save_model(model_dir, model, parameters, opt)
+        save_model(model_dir, cpu_model, parameters, opt)
 
     end
 
