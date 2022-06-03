19060977e5dd58d55c406b7dcb39db3028d95f25
mxkopy <mxkopylov@gmail.com>
2022-06-02 22:04:36 -0700

    dense_ifft is now dense_fft, and now works

--- AudioVAE.jl
+++ AudioVAE.jl
@@ -1,7 +1,7 @@
 using Flux, FFTW, SliceMap
 
 
-struct DenseIFFT
+struct DenseFFT
 
     W::Array{Complex}
     b::Array{Complex}
@@ -9,36 +9,43 @@ struct DenseIFFT
 end
 
 
-function dense_ifft( layer::DenseIFFT, x::Vector{Complex} )
+function dense_ifft( layer::DenseFFT, data::AbstractArray{<: Real, 1} )
 
-    frq = ( x * layer.W ) .+ layer.b
+    x   = FFTW.fft( data )
+    frq = ( transpose( layer.W ) * x ) .+ layer.b
     out = FFTW.ifft( frq ) .|> real
+
+    return out
 
 end
 
 
-function (layer::DenseIFFT)(data::Array{Complex})
+function (layer::DenseFFT)(data::Array{<: Real} )
 
     return slicemap( x -> dense_ifft( layer, x ), data, dims=1 )
 
 end
 
 
-init_complex_array( shape, init ) = Complex.( init( reduce(*, shape) ), init( reduce(*, shape) ) ) |> x -> reshape(x, shape )
+init_complex_array( shape, init ) = Complex.( init( reduce(*, shape) ), init( reduce(*, shape) ) ) |> x -> reshape( x, shape )
 
 
-DenseIFFT( dim_in, dim_out; init=Flux.glorot_normal ) = DenseIFFT( init_complex_array( ( dim_in, dim_out ), init ), init_complex_array( dim_out, init ) )
+DenseFFT( dim_in::Int, dim_out::Int; init=Flux.glorot_normal ) = DenseFFT( init_complex_array( ( dim_in, dim_out ), init ), init_complex_array( dim_out, init ) )
 
 
-Flux.@functor IFFTKernel
+Flux.@functor DenseFFT
+
+
+
+
+
 
 
 function audio_coder( input_size, in_channels, out_channels, kernel, stride, conv_type )
 
     return Chain(
 
-        FFTW.fft, 
-        IFFTKernel( ( input_size, 1 ) ), 
+        DenseFFT( input_size, input_size ), 
         conv_type( (kernel, kernel), in_channels=>out_channels, stride=stride, pad=SamePad() )
 
     )
